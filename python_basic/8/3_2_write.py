1e31637ce904b71709bfb89fdc7237e579c565fe
korea <koreanbulbasaur99@naver.com>
2023-04-25 17:02:44 +0900

    'write'

---
+++ python_basic/8/3_2_write.py
@@ -0,0 +1,5 @@
+score_file = open(
+    r"C:\Private_Projects\nadocoding\python_basic\8\score.txt", "a", encoding="utf8")
+score_file.write("과학 : 35")
+score_file.write("\n사회 : 15")
+score_file.close()
