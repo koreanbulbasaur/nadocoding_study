b19fb4e3183008ede7ad8d7b075f1677e2fe7507
korea <koreanbulbasaur99@naver.com>
2023-04-25 11:53:03 +0900

    'slicing'

---
+++ python_basic/4/2_slicing.py
@@ -0,0 +1,10 @@
+vito = "990101-1234567"
+
+print("성별 : " + vito[7])
+print("연 : " + vito[0:2])
+print("월 : " + vito[2:4])
+print("일 : " + vito[4:6])
+
+print("생년월일 : " + vito[:6])
+print("뒤 7자리 : " + vito[7:])
+print("뒤 7자리 (뒤에부터) : " + vito[-7:])
