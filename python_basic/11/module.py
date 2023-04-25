f0bf2e8e714f81296c154aa06a5ea7eedeb6dffe
korea <koreanbulbasaur99@naver.com>
2023-04-26 08:52:28 +0900

    'module'

---
+++ python_basic/11/module.py
@@ -0,0 +1,11 @@
+# from theater_module import price, price_morning
+# import theater_module
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
+
+# price(5)
+# price_morning(6)
+
+from theater_module import price_soldier as price
+price(5)
---
+++ python_basic/11/theater_module.py
@@ -0,0 +1,10 @@
+def price(people):
+    print("{}명 가격은 {}원 입니다.".format(people, people * 10000))
+
+
+def price_morning(people):
+    print(f"{people}명 조조 할인 가격은 {people * 6000}원 입니다.")
+
+
+def price_soldier(people):
+    print(f"{people}명 군인 할인 가격은 {people * 3000}원 입니다.")
