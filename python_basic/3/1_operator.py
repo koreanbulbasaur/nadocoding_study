50108658c563fb9df090d18e7076a780a6ea4c80
korea <koreanbulbasaur99@naver.com>
2023-04-25 11:21:12 +0900

    'plus'

--- python_basic/3/1_operator.py
+++ python_basic/3/1_operator.py
@@ -17,3 +17,15 @@ print(5 <= 5)
 print(3 == 4)
 print(5 == 5)
 print(3+4 == 7)
+
+print(1 != 3)
+print(not (1 != 3))
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) or (3 > 5))
+print((3 > 0) | (3 > 5))
+
+print(5 > 4 > 3)
+print(5 > 4 > 7)
