40a9c294377a246ecab17c58d32ec05bd4fc9eed
korea <koreanbulbasaur99@naver.com>
2023-04-25 12:25:11 +0900

    '\'

---
+++ python_basic/4/5_escape_c.py
@@ -0,0 +1,20 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+print("나는 '펭귄'이다.")
+print('I am a "penguin"')
+print("나는 \"펭귀니\"아니야")
+print('I am not a \'penguin')
+
+# \\ : 문장내에서 \
+print("C:\\Private_Projects\\nadocoding\\python_basic\\4\\5_escape_c.py")
+
+# \r : 커서 맨 앞으로 이동
+print("Led Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Ledd\bApple")
+
+# \t : 탭
+print("Led\tApple")
