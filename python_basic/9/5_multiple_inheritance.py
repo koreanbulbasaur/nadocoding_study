e4e8403b82c185c1eadd32a083a4defe45eff6d8
korea <koreanbulbasaur99@naver.com>
2023-04-25 18:24:01 +0900

    'm_i'

---
+++ python_basic/9/5_multiple_inheritance.py
@@ -0,0 +1,50 @@
+# 일반 유닛
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# 공격 유닛
+
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print(
+            f"{self.name} : {location} 방향으로 적군을 공격 합니다. [공격력 {self.damage}]")
+
+    def damaged(self, damage):
+        print(f"{self.name} : {damage} 데미지를 입었습니다.")
+        self.hp -= damage
+        print(f"{self.name} : 현재 체력은 {self.hp}")
+        if self.hp <= 0:
+            print(f"{self.name} : 파괴되었습니다.")
+
+# 공중 유닛
+
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print(f"{name} : {location} 방향으로 날아갑니다. [속도{self.flying_speed}]")
+
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+firebat1.damaged(25)
+firebat1.damaged(25)
+
+valkyrie = FlyableAttackUnit("발키리",  200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
