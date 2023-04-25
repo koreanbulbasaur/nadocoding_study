4f6f8c4c31f8a10189e747d2af712e927c1834d6
korea <koreanbulbasaur99@naver.com>
2023-04-25 14:54:38 +0900

    'for_one'

---
+++ python_basic/6/5_for_one.py
@@ -0,0 +1,7 @@
+students = [1, 2, 3, 4, 5]
+students = [i+100 for i in students]
+print(students)
+
+students = ['minu', 'dragon', 'tiger']
+students = [i.upper() for i in students]
+print(students)
