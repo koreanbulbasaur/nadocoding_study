824adfda76ef31648d982f0c1644bb16d5708ae6
korea <koreanbulbasaur99@naver.com>
2023-04-25 11:11:08 +0900

    'I

---
+++ python_basic/2/4_variable.py
@@ -0,0 +1,10 @@
+# 자기소개
+name = "현용범"
+age = 33
+hobby = "그림그리기"
+food = "감자튀김"
+is_adult = age >= 20
+
+print("안녕하세요. 저는 " + name + " 입니다.")
+print(str(age) + "살 이구여 취미는 " + hobby + " 좋아하는 음식은 " + food + '에요')
+print(name + "는 어른일까요? " + str(is_adult))
