24aeee8697db45f67be67a88cfe5582f0869320f
korea <koreanbulbasaur99@naver.com>
2023-04-25 17:47:29 +0900

    'class'

---
+++ python_basic/9/1_class.py
@@ -0,0 +1,12 @@
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print(f"{self.name} 유닛이 생성 되었습니다.")
+        print(f"체력 {self.hp}, 공격력 {self.damage}")
+
+
+marine1 = Unit("마린", 40, 5)
+marine2 = Unit("마린", 40, 5)
+tank = Unit("탱크", 150, 35)
