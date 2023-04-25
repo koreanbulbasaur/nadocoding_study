e26cbea4b36ba29c8198bf17dd4bea413b0d5d18
korea <koreanbulbasaur99@naver.com>
2023-04-25 14:00:05 +0900

    'tuple'

---
+++ python_basic/5/3_tuple.py
@@ -0,0 +1,13 @@
+menu = ("돼지국밥", "고기국수", "전복내장죽")
+print(menu[0])
+print(menu[1])
+
+# menu.add("초밥") 오류남
+
+name = "현용범"
+age = 33
+hobby = "그림그리기"
+print(name, age, hobby)
+
+(name_1, age_1, hobby_1) = ("이현", 33, "코딩")
+print(name_1, age_1, hobby_1)
