fb01bea894d41f8aaf5dc13eaac3b4c3d20afaf7
korea <koreanbulbasaur99@naver.com>
2023-04-25 11:18:23 +0900

    'operator'

---
+++ python_basic/3/1_operator.py
@@ -0,0 +1,19 @@
+print(1 + 1)
+print(4-6)
+print(4*2)
+print(4/2)
+
+print(2**3)
+print(5 % 3)
+print(10 % 3)
+print(5//3)
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 4)
+print(5 == 5)
+print(3+4 == 7)
