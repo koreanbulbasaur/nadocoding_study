56971299a2315eac5a7a86903b8a8a5fb9d18dd3
korea <koreanbulbasaur99@naver.com>
2023-04-25 12:03:03 +0900

    'iamsleep'

---
+++ python_basic/4/3_string_func.py
@@ -0,0 +1,16 @@
+name = "Vito Frost Jahad"
+print(name.lower())
+print(name.upper())
+print(name[0].isupper())
+print(len(name))
+print(name.replace("Frost", "Gorogoa"))
+
+index = name.index("o")
+print(index)
+index = name.index("o", index + 1)
+print(index)
+
+print(name.find("Frost"))
+print("hi")
+
+print(name.count("a"))
