1ac19e28f04202397c056c2e783d28d12f1a2bbe
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-03 10:07:50 +0900

    button

--- gui_basic/2_button.py
+++ gui_basic/2_button.py
@@ -2,9 +2,32 @@ from tkinter import *
 
 root = Tk()
 root.title("Vito GUI")
-# root.geometry("640x480")  # 가로 * 세로
-root.geometry("640x480+100+300")  # 가로 * 세로 + x좌표 + y좌표
 
-root.resizable(False, False)  # 창 크기 변경 불가
+btn1 = Button(root, text='버튼1')
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=20, text='버튼2')
+btn2.pack()
+
+btn3 = Button(root, padx=20, pady=5, text='버튼3')
+btn3.pack()
+
+btn4 = Button(root, width=20, height=5, text='버튼4')
+btn4.pack()
+
+btn5 = Button(root, fg='red', bg='yellow', text="버튼5")
+btn5.pack()
+
+photo = PhotoImage(file="gui_basic/img.png")
+btn6 = Button(root, image=photo)
+btn6.pack()
+
+
+def btncmd():
+    print("버튼이 클릭됨")
+
+
+btn7 = Button(root, text="동작하는 버튼", command=btncmd)
+btn7.pack()
 
 root.mainloop()
