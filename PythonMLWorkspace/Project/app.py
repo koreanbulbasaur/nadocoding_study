e4d56c260bcc5aa40f19a205dee2c0255fd872c3
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-30 13:37:36 +0900

    45 project

--- PythonMLWorkspace/Project/app.py
+++ PythonMLWorkspace/Project/app.py
@@ -5,3 +5,25 @@ from tmdbv3api import Movie, TMDb
 movie = Movie()
 tmdb = TMDb()
 tmdb.api_key = '60253fee74455f6316932b26820c3aad'
+
+def get_recommendations(title):
+    idx = movies[movies['title'] == title].index[0]
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_scores = sim_scores[1:11]
+
+    movie_indices = [i[0] for i in sim_scores]
+
+movies = pickle.load(open('movies.pickle', 'rb'))
+cosine_sim = pickle.load(open('cosine_sim.pickle', 'rb'))
+
+st.set_page_config(layout='wide')
+st.header('Vitoflix')
+
+movie_list = movies['title'].values
+title = st.selectbox('Choose a movie you like', movie_list)
+if st.button('Recommend'):
+    images, titles = get_recommendations(title)
